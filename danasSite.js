e0db208549117866a21c812ed6f2928a76389b5e
YuryPoisik <112342285+YuryPoisik@users.noreply.github.com>
2023-03-09 23:35:46 +0100

    Add files via upload

--- danasSite.js
+++ danasSite.js
@@ -1,5 +1,6 @@
 
 const button = document.querySelector('button');
 button.addEventListener('click', () => {
-    document.getElementById("gifka-brand").src = "danas_tongue_01.gif"
+    button.ariaExpanded !== "true" ? document.getElementById("gifka-brand").src = "danas_tongue_01.gif" :
+        document.getElementById("gifka-brand").src = "Artefakt_animation_logo_v005_transp.png"
 });
